df449f0d2665037af31ae03be0fbb0630a9bbe59
txd481 <txd481@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-02-03 19:30:45 +0000

    The UnitTesting class for Algorithms

---
+++ SortAlog/src/testing/SortAlgosTest.java
@@ -0,0 +1,58 @@
+package tests;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import core.SortAlgos;
+
+
+public class SortAlgosTest {
+	SortAlgos SortAlgos = new SortAlgos();
+
+	@Test
+	public void BubbleSortEmptyLyst_ShouldReturnEmptyList() {
+		Assert.assertTrue(Arrays.equals(new int[]{}, SortAlgos.Bubble(new int[]{})));
+	}
+	@Test
+	public void BubbleSortOneElement_ShouldReturnTheElement() {
+		Assert.assertTrue(Arrays.equals(new int[]{1}, SortAlgos.Bubble(new int[]{1})));
+	}
+	@Test
+	public void BubbleSortTwoElementsInOrder_ShouldReturnSameList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,2}, SortAlgos.Bubble(new int[]{1,2})));
+	}
+	@Test
+	public void BubbleSortTwoElementsInReverseOrder_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1, 2}, SortAlgos.Bubble(new int[]{2, 1})));
+	}
+	@Test
+	public void BubbleSortTwoEqualElements_ShouldReturnElementsInOrder() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,1}, SortAlgos.Bubble(new int[]{1,1})));
+	}
+	@Test
+	public void BubbleSortThreeElementsInOrder_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,2,3}, SortAlgos.Bubble(new int[]{1,2,3})));
+	}
+	@Test
+	public void BubbleSortThreeElementsLastTwoSwaped_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,2,3}, SortAlgos.Bubble(new int[]{1,3,2})));
+	}
+	@Test
+	public void BubbleSortThreeElementsFirstTwoSwaped_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,2,3}, SortAlgos.Bubble(new int[]{2,1,3})));
+	}
+	@Test
+	public void BubbleSortThreeElementsInReverseOrder_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{1,2,3}, SortAlgos.Bubble(new int[]{3,2,1})));
+	}
+	@Test
+	public void BubbleSortNElementsRandomOrder_ShouldReturnSortedList() {
+		Assert.assertTrue(Arrays.equals(new int[]{0,1,2,3,4,5,6,7,8,9}, SortAlgos.Bubble(new int[]{1,5,0,7,6,2,8,9,4,3})));
+	}
+	
+
+}

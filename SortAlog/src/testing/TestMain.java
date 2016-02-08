1afeb3153f2908b87841f810ac39d2c0b2d50193
knn453 <knn453@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-02-08 00:39:20 +0000

    Finished QuickSort testing

--- SortAlog/src/testing/TestMain.java
+++ SortAlog/src/testing/TestMain.java
@@ -10,22 +10,25 @@ public class TestMain {
 	 */
 	public static void main(String[] args) {
 		SortAlgos sorts = new SortAlgos();
-		int[] array = new int[] {5, 7, 20, 13, 8, 2, 6, 19, 10, 17};
-
+		//int[] array = new int[] {5, 7, 20, 13, 8, 2, 6, 19, 10, 17};
+		int[] array = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 		
 		ArrayList<int[]> sorted = new ArrayList<int[]>();
 		sorted = sorts.bubbleSort(array);
+		//sorted = sorts.quickSort(array);
 		int[] result= sorted.get(sorted.size()-1);
 		
 		//Print the arraylist of arraylists
-		System.out.println(Arrays.toString(result));
-		
+		//System.out.println(Arrays.toString(result));
+		int rotCount = 0;
 		for(int[] innerList : sorted) {
 		    for(Integer number : innerList) {
 		        System.out.print(number + ", ");
 		    }
+		    rotCount++;
 		    System.out.println();
 		}
+		System.out.println(rotCount);
 	}
 
 }

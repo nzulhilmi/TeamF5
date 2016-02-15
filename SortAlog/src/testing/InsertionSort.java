4bc4df9df032ee3cce2c8d60d756e8b7d157ae53
nzn448 <nzn448@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-02-15 22:30:32 +0000

    changed insertion sort arraylist to array

--- SortAlog/src/testing/InsertionSort.java
+++ SortAlog/src/testing/InsertionSort.java
@@ -1,6 +1,7 @@
 package testing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Implementation of Insertion Sort algorithm.
@@ -13,58 +14,40 @@ public class InsertionSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ArrayList<Integer> test = new ArrayList<Integer>();
-		test.add(70);
-		test.add(4);
-		test.add(40);
-		test.add(50);
-		test.add(2);
-		test.add(13);
-		test.add(27);
-		test.add(1);
-		test.add(38);
-		test.add(6);
+		int[] test = {7, 4, 40, 50, 2, 13, 27, 1, 38, 6};
 		// [7, 4, 40, 50, 2, 13, 27, 1, 38, 6]
 		// result = [1, 2, 4, 6, 7, 13, 27, 38, 50, 70]
 		algorithm(test);
-		
-		int n = algorithm(test).size() - 1;
-		System.out.println(algorithm(test).get(n));
 	}
 	
 	/**
 	 * Insertion sort algorithm.
-	 * @param a
-	 * @return ArrayList<ArrayList<Integer>> steps. Returns step
+	 * @param input
+	 * @return ArrayList<int[]> steps. Returns step
 	 * by step array list.
 	 */
-	public static ArrayList<ArrayList<Integer>> algorithm(ArrayList<Integer> a) {
+	public static ArrayList<int[]> algorithm(int[] input) {
 		
-		ArrayList<ArrayList<Integer>> steps = new ArrayList<ArrayList<Integer>>();
-		steps.add(a);
+		ArrayList<int[]> steps = new ArrayList<int[]>();
+		steps.add(input.clone()); //add the input array to steps
 		
 		//the algorithm for insertion sort
-		int n = a.size();
+		int n = input.length;
 		for(int i = 1; i < n; i++) {
-			ArrayList<Integer> temporary = new ArrayList<Integer>();
-			
-			int key = a.get(i); //element to be compared
+						
+			int key = input[i]; //element to be compared
 			
 			int j = i - 1;
-			while( (j > -1) && (a.get(j) > key)) {
-				a.set(j+1, a.get(j));
+			while( (j > -1) && (input[j] > key)) {
+				input[j+1] = input[j];
 				j--;
 			}
-			a.set(j+1, key);
-			
-			//add a to temporary list
-			for(int k = 0; k < a.size(); k++) {
-				temporary.add(a.get(k));
-			}
+			input[j+1] = key;
 			
 			//add to steps array list
-			steps.add(temporary);
+			steps.add(input.clone());
 		}
+		
 		return steps;
 	}
 }

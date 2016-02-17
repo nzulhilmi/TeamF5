e4b4f8412be465b65bb97c0cd912e13af11968b0
nzn448 <nzn448@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-02-17 14:19:52 +0000

    replaced predefined inputs with sorting algorithm

--- SortAlog/src/testing/algComponent.java
+++ SortAlog/src/testing/algComponent.java
@@ -11,16 +11,15 @@ public class algComponent extends JPanel
 {
 	public algComponent(sortingAlgs sort)
 	{	
-		ArrayList<int[]> steps = new ArrayList<int[]>();
-		steps.add(new int[] {1,1,1,1,1});
-		ArrayList<int[]> steps1 = new ArrayList<int[]>();
-		steps1.add(new int[] {5,4,3,2,1});
-		steps1.add(new int[] {4,5,3,2,1});
-		steps1.add(new int[] {3,4,5,2,1});
-		steps1.add(new int[] {2,3,4,5,1});
-		steps1.add(new int[] {1,2,3,4,5});
+		ArrayList<int[]> steps = new ArrayList<int[]>();	
 		
-		algModel model = new algModel(sort, steps, null);
+		//31, 56, 87, 12, 98, 22, 43, 131, 76, 5
+		int[]input = {31, 56, 87, 12, 98, 22, 43, 131, 76, 5};
+		
+		steps = SortAlgos.bubbleSort(input);
+		
+		
+		algModel model = new algModel(sort, steps, null); //------------steps
 		controlsPanel controlspanel = new controlsPanel(model);
 		setLayout(new BorderLayout());
 
@@ -32,14 +31,14 @@ public class algComponent extends JPanel
 		title.setHorizontalAlignment(getWidth()/2);
 		title.setFont(new Font("Helvetica", Font.BOLD, 20));
 		
-		visualiser vis = new visualiser(model, getWidth(), getHeight(), steps, 0);
+		visualiser vis = new visualiser(model, getWidth(), getHeight(), steps, 0); //------------steps
 		model.setVis(vis);
 		add(title, BorderLayout.NORTH);
 		add(controlspanel, BorderLayout.SOUTH);
 		add(vis, BorderLayout.CENTER);
 		//model.addObserver(e->);
-		model.setCurrentList(steps1);
-		vis.setSorted(steps1);
+		model.setCurrentList(steps); //----------steps1
+		vis.setSorted(steps); //----------------steps1
 		vis.setCurrentIndex(1);
 		//vis.setCurrentList(steps1.get(1));
 		

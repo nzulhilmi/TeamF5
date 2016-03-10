d0e515908994d29ff12b1fa56f2f70952809adcd
eju455 <eju455@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-03-10 10:56:52 +0000

    Explanations updated

--- SortAlog/src/javaFx/ExplanationPane.java
+++ SortAlog/src/javaFx/ExplanationPane.java
@@ -1,6 +1,7 @@
 package javaFx;
 
-import javax.print.attribute.standard.RequestingUserName;
+import java.nio.channels.ShutdownChannelGroupException;
+import java.util.stream.IntStream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,8 +10,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class ExplanationPane extends GridPane {
+	public TextArea explanation = new TextArea();
 	public ExplanationPane() {
 		super();
 		// this.setAlignment(Pos.TOP); // location of the table
@@ -25,7 +28,7 @@ public class ExplanationPane extends GridPane {
 		ObservableList<String> options = FXCollections.observableArrayList("Bubble", "Quick", "PlaceHolder");
 		final ComboBox<String> comboBox = new ComboBox<String>(options);
 
-		TextArea explanation = new TextArea();
+
 		explanation.setWrapText(true);
 		explanation.setText("Select a sort from the drop down list to display a discrition of how it works");
 		comboBox.setOnAction(e -> explanation.setText(TextExplanation(comboBox.getValue())));
@@ -33,6 +36,7 @@ public class ExplanationPane extends GridPane {
 		this.add(ExplanationTitle, 0, 0, 2, 1);
 		this.add(comboBox, 1, 1);
 		this.add(explanation, 1, 2);
+		// explanation.setVisible(false);
 		// this.add(insertion, 1, 3);
 		// this.add(advanced, 1, 4);
 	}
@@ -41,9 +45,13 @@ public class ExplanationPane extends GridPane {
 		String text = "Nothing";
 		switch (sort) {
 		case "Bubble":
-			return text = "this is bubble sort";
+			return text = "Bubble Sort \n Bubble sort compares the two elements which are next to each other and swapping them to put them in accending order.";
 		case "Quick":
-			return text = "This is Quick Sort";
+			return text = "Quick Sort \n Quick sort firstly picks a pivot element. All other element are then compared to the pivot and placed to the left if they are smaller or to the right if they are larger, splitting the array. \n The pivot is then marked as sorted. \n The algorithm then runs simltaniously on both the left and right sides.";
+		case "PlaceHolder":
+			return text = "This is placeholder Sort";
+			//case "Quick":
+			//return text = "This is Quick Sort";
 		default:
 			break;
 		}

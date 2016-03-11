a63ba049a7c723855d1a1532444fe2906099f3ac
nzn448 <nzn448@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-03-11 09:30:39 +0000

    added separate advanced menu class

---
+++ SortAlog/src/javaFx/AdvancedMenu.java
@@ -0,0 +1,149 @@
+package javaFx;
+
+import java.util.Arrays;
+import java.util.Random;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+public class AdvancedMenu extends BorderPane{
+    private Stage stage;
+    private BorderPane border;
+    private int[] input =  {10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    public AdvancedMenu(Stage stage, BorderPane border) {
+	this.setId("advanced");
+	this.stage = stage;
+	this.border = border;
+
+	TilePane advancedTop = new TilePane();
+	advancedTop.setPadding(new Insets(2, 10, 2, 10));
+	advancedTop.setVgap(5);
+	advancedTop.setHgap(5);
+	advancedTop.setAlignment(Pos.CENTER);
+
+	GridPane advancedBottom = new GridPane();
+	advancedBottom.setPadding(new Insets(2, 10, 2, 10));
+	advancedBottom.setVgap(5);
+	advancedBottom.setHgap(5);
+	advancedBottom.setAlignment(Pos.CENTER);
+
+	Label advancedLabel = new Label("Choose input: ");
+
+	TextArea customInput = new TextArea(); //text area to fill custom inputs
+	customInput.setPrefSize(200, 10);
+	customInput.setMaxHeight(10);
+
+	ToggleGroup group = new ToggleGroup();
+
+	//buttons for custom inputs
+	RadioButton sorted = new RadioButton("Sorted");
+	sorted.setToggleGroup(group);
+	//sorted.setSelected(true);
+	sorted.setOnAction(e -> {
+	    input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	    customInput.setText("1,2,3,4,5,6,7,8,9,10");
+	    System.out.println("sorted input");
+	});
+
+	RadioButton random = new RadioButton("Random");
+	random.setToggleGroup(group);
+	random.setOnAction(e -> {
+	    System.out.println("random input");
+	    shuffleArray(input); //shuffle the array
+	    String s = Arrays.toString(input); //convert array to string
+	    s = s.replaceAll("\\s+", ""); //remove all the white spaces
+	    s = s.substring(1, s.length()-1); //remove the '[' and ']'
+	    customInput.setText(s);
+	});
+
+	RadioButton reverse = new RadioButton("Reversed");
+	reverse.setToggleGroup(group);
+	reverse.setOnAction(e -> {
+	    input = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	    customInput.setText("10,9,8,7,6,5,4,3,2,1");
+	    System.out.println("reversed input");
+	});
+
+	Label customInputLabel = new Label("Insert custom input. e.g 1,2,3,4,5,6,7,8,9,10");
+
+	Button submit = new Button("Submit");
+	submit.setOnAction(e -> {
+	    String getInput = customInput.getText(); //get input from text area
+	    getInput = getInput.replaceAll("\\s+", ""); //remove all the white spaces
+	    String s = getInput.replaceAll(",", "");
+	    String regex = "[0-9]+";
+	    String[] split = getInput.split(","); //split the string into elements separated by ','
+	    boolean b1 = true;
+	    for(int i = 0; i < getInput.length()-2; i++) { //check if there's two commas side by side
+		if(getInput.charAt(i) == ',' && getInput.charAt(i+1) == ',') {
+		    b1 = false;
+		}
+	    }
+	    if(s.matches(regex) && split.length == 10 && b1 && getInput.charAt(0) != ',') {
+		int[] output = new int[10];
+		for(int i = 0; i < split.length; i++) {
+		    output[i] = Integer.parseInt(split[i]); //copy the elements into another array
+		}
+		input = output;
+	    }
+	    else {
+		customInput.setText("Invalid input. Try again.");
+	    }
+	});
+
+	Button closeAdv = new Button("Close");
+	closeAdv.setOnAction(e -> {
+	    removeAdvanced();
+	    resizeStage();
+	});
+
+	advancedTop.getChildren().add(advancedLabel);
+	advancedTop.getChildren().add(sorted);
+	advancedTop.getChildren().add(reverse);
+	advancedTop.getChildren().add(random);
+	advancedBottom.add(customInputLabel, 0, 0);
+	advancedBottom.add(customInput, 0, 1);
+	advancedBottom.add(submit, 1, 1);
+	advancedBottom.add(closeAdv, 2, 1);
+
+	this.setTop(advancedTop);
+	this.setCenter(advancedBottom);
+    }
+
+    public void resizeStage() {
+	stage.sizeToScene();
+    }
+
+    public void removeAdvanced() {
+	border.getChildren().remove(border.lookup("#advanced"));
+    }
+
+    public void shuffleArray(int[] array)
+    {
+	int index;
+	Random random = new Random();
+	for (int i = array.length - 1; i > 0; i--)
+	{
+	    index = random.nextInt(i + 1);
+	    if (index != i)
+	    {
+		array[index] ^= array[i];
+		array[i] ^= array[index];
+		array[index] ^= array[i];
+	    }
+	}
+    }
+
+    public int[] getInput() {
+	return input;
+    }
+}

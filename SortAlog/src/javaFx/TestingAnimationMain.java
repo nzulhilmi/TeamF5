c70651396a07f0af3b4b1db0ee60e49beca2bcdb
knn453 <knn453@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-03-14 11:26:36 +0000

    made a new testing class for bug fixing the algorithms

---
+++ SortAlog/src/javaFx/TestingAnimationMain.java
@@ -0,0 +1,19 @@
+package javaFx;
+
+public class TestingAnimationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {3,1,2,4,8,7,9,0};
+		SortAlgos a = new SortAlgos("Selection", array);
+
+		System.out.println("Start: ");
+		for(int i = 0;i<a.sorted.size();i++){
+			System.out.println();
+			for(int j=0;j<a.sorted.get(i).length;j++){
+				System.out.print(a.sorted.get(i)[j] + ", ");
+			}
+		}
+	}
+
+}

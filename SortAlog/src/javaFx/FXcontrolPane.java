642b7655ddf56328676720bdbd546d0004055ac8
knn453 <knn453@ed29822c-1063-41ea-9c20-242838caa4fb>
2016-03-22 15:26:07 +0000

    Formatting + comments + JavaDoc

--- SortAlog/src/javaFx/FXcontrolPane.java
+++ SortAlog/src/javaFx/FXcontrolPane.java
@@ -10,34 +10,60 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
+// TODO: Auto-generated Javadoc
 /**
- * Controls panel for the visualisation
+ * Controls panel for the visualisation.
  *
- * @author ElliottUpton
- *
+ * @author ElliottUpton, Kiril Nedkov
  */
 
 public class FXcontrolPane extends GridPane {
+
+	/** The model. */
 	algModel model;
+
+	/** The button disabled play flag. */
 	public Boolean btnDisabledPlay = false;
+
+	/** The button disabled stop. */
 	public Boolean btnDisabledStop = false;
+
+	/** The button disabled forward flag. */
 	public Boolean btnDisabledForw = false;
+
+	/** The button disabled back flag. */
 	public Boolean btnDisabledBack = false;
+
+	/** The forward button. */
 	private Button forward = new Button();
+
+	/** The back button . */
 	private Button back = new Button();
+
+	/** The play button. */
 	private Button play = new Button();
+
+	/** The pause button. */
 	private Button pause = new Button();
+
+	/** The close button. */
 	Button close = new Button();
+
+	/** The speed. */
 	public double speed = 1;
 
-	public FXcontrolPane(algModel model, TextArea logText) { // pass the model
-																// so it acts on
-																// the same
-																// thing
+	/**
+	 * Instantiates a new FXcontrol pane.
+	 *
+	 * @param model
+	 *            the model
+	 * @param logText
+	 *            the log text
+	 */
+	public FXcontrolPane(algModel model, TextArea logText) {
+		// set the model
 		this.model = model;
-		// this.setBackground(new Background(new
-		// BackgroundFill(Color.ALICEBLUE,CornerRadii.EMPTY, Insets.EMPTY)));
-		// make the slider, needs an action listener
+		// make the slider and give it the needed properties
 		Slider slider = new Slider();
 		slider.setSnapToTicks(true);
 		slider.setShowTickMarks(true);
@@ -46,74 +72,90 @@ public class FXcontrolPane extends GridPane {
 		slider.setMax(2);
 		slider.setValue(1);
 		slider.setMajorTickUnit(.5);
+		// add a listener on the slider
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
 				speed = new_val.doubleValue();
 			}
 		});
 
-		// buttons creation
+		// create the back button
 		back = new Button("|<<");
+		// set the action
 		back.setOnAction(e -> {
 			if (btnDisabledBack) {
+				// log failed attempt
 				logText.appendText("\n Please wait for the action to complete before starting another");
 				System.out.println("Please wait for the action to complete before starting another");
 			} else {
+				// call function for back and disable back button
 				disableBack();
-				//back.setDisable(true);
 				model.goBack();
 			}
-			//back.setDisable(btnDisabled);
 		});
+		// create the play button
 		play = new Button("Play");
+		// set the action
 		play.setOnAction(e -> {
 			if (btnDisabledPlay) {
+				// log the failed attempt
 				logText.appendText("\n Please wait for the action to complete before starting another");
 				System.out.println("Please wait for the action to complete before starting another");
 			} else {
+				// call function for play and disable play button
 				disablePlay();
 				System.out.println("Play");
 				logText.appendText("\n Play");
 				model.play();
 			}
-			//play.setDisable(btnDisabled);
 		});
-
+		// create the stop button
 		pause = new Button("Stop");
-		pause = new Button("Stop");
+		// set the action
 		pause.setOnAction(e -> {
-			// System.out.println("pause");
+			// log the stop
 			logText.appendText("\n Stop");
+			// call the stop function
 			model.stop();
+			// set flag to the value of btnDisabledPlay
 			play.setDisable(btnDisabledPlay);
 		});
-
+		// create the forwards button
 		forward = new Button(">>|");
+		// set the action
 		forward.setOnAction(e -> {
 			if (btnDisabledForw) {
+				// log the failed attempt
 				logText.appendText("\n Please wait for the action to complete before starting another");
 				System.out.println("Please wait for the action to complete before starting another");
 			} else {
+				// disable the forward button
 				disableForward();
-				// forward.setDisable(btnDisabled);
+				// call the forward method
 				model.goForward();
 			}
-			//forward.setDisable(btnDisabled);
 		});
-
+		// create the close button
 		close = new Button("Close");
+		// set the action
 		close.setOnAction(e -> {
+			// get the id
 			int intID = this.model.getID();
+			// make it a string
 			String stringID = Integer.toString(intID);
+			// append #
 			String ID = "#" + stringID;
-			// System.out.println("close");
+			// log the close
 			logText.appendText("\n Close");
+			// stop the timer in the model
 			model.stop();
+			// remove the visualiser by ID
 			FXmainMenuGUI.removeVis(ID);
 		});
 
-		// speed label
+		// create the speed label
 		Text speed = new Text("Speed");
+		// set the properties
 		speed.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
 
 		// add all the controls into the grid
@@ -124,67 +166,100 @@ public class FXcontrolPane extends GridPane {
 		this.add(pause, 3, 2);
 		this.add(forward, 4, 2);
 		this.add(close, 6, 2);
-	}/*
-		 * public void enableBtn(){ btnDisabled = false;
-		 * forward.setDisable(false); back.setDisable(false); } public void
-		 * disableBtn(){ btnDisabled = true; forward.setDisable(true);
-		 * back.setDisable(true); }
-		 */
+	}
 
+	/**
+	 * Disable back button.
+	 */
 	public void disableBack() {
 		btnDisabledBack = true;
 		back.setDisable(true);
 	}
 
+	/**
+	 * Disable forward button.
+	 */
 	public void disableForward() {
 		btnDisabledForw = true;
 		forward.setDisable(true);
 	}
 
+	/**
+	 * Disable play button.
+	 */
 	public void disablePlay() {
 		btnDisabledPlay = true;
 		play.setDisable(true);
 	}
 
+	/**
+	 * Disable stop button.
+	 */
 	public void disableStop() {
 		btnDisabledStop = true;
 		pause.setDisable(true);
 	}
 
+	/**
+	 * Enable forward button.
+	 */
 	public void enableForward() {
 		btnDisabledForw = false;
 		forward.setDisable(false);
 	}
 
+	/**
+	 * Enable back button.
+	 */
 	public void enableBack() {
 		btnDisabledBack = false;
 		back.setDisable(false);
 	}
 
+	/**
+	 * Enable play button.
+	 */
 	public void enablePlay() {
 		btnDisabledPlay = false;
 		play.setDisable(false);
 	}
 
+	/**
+	 * Enable stop button.
+	 */
 	public void enableStop() {
 		btnDisabledStop = false;
 		pause.setDisable(false);
 	}
 
+	/**
+	 * Click play button.
+	 */
 	public void clickPlay() {
-		// forward.fire() //just to test if it works
 		play.fire();
 	}
 
+	/**
+	 * Click stop button.
+	 */
 	public void clickStop() {
 		pause.fire();
 	}
 
+	/**
+	 * Click close button.
+	 */
 	public void clickClose() {
 		close.fire();
 	}
 
+	/**
+	 * Gets the speed.
+	 *
+	 * @return the speed
+	 */
 	public double getSpeed() {
-		return (2.2-speed);
+		// remove the delay from a number to get the speed setting
+		return (2.2 - speed);
 	}
 }
